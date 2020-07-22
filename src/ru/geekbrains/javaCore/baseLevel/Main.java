f29fe5644e7976cb0d29f56cd904f3bc3ebc054b
IgorNiNo <vis_74@mail.ru>
2020-07-22 21:25:03 +0300

    developHw2

--- src/ru/geekbrains/javaCore/baseLevel/Main.java
+++ src/ru/geekbrains/javaCore/baseLevel/Main.java
@@ -10,49 +10,59 @@ public class Main {
         int[] num1 = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
         int[] num2 = new int[8];
         int[] num3 = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
-        int[][] num4 = new int[5][5];
+        int[][] num4 = new int[7][7];
         int[] num5 = { 21, 5, 3, 2, 11, 4, 5, 34, 14, 8, 9, 7 };
         int[] num6 = { 1, 5, 3, 2, 11 };
+        int[] num7 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 
         //задание 1
         System.out.println("Задание 1.\nИсходный массив данных:");
         System.out.println(Arrays.toString(num1));
-        System.out.println("Инвертированный массив данных:");
         invertArr(num1);
-        System.out.println();
+        System.out.println("Инвертированный массив данных:");
+        System.out.println(Arrays.toString(num1));
 
         //задание 2
-        System.out.println("Задание 2.\nИсходный массив данных:");
+        System.out.println("\nЗадание 2.\nИсходный массив данных:");
         System.out.println(Arrays.toString(num2));
-        System.out.println("Заполненный массив данных:");
         fillArr(num2);
-        System.out.println();
+        System.out.println("Заполненный массив данных:");
+        System.out.println(Arrays.toString(num2));
 
         //задание 3
-        System.out.println("Задание 3.\nИсходный массив данных:");
+        System.out.println("\nЗадание 3.\nИсходный массив данных:");
         System.out.println(Arrays.toString(num3));
-        System.out.println("Заполненный массив данных:");
         multiplyArr(num3);
-        System.out.println();
+        System.out.println("Заполненный массив данных:");
+        System.out.println(Arrays.toString(num3));
 
         //задание 4
-        System.out.println("Задание 4.\nЗаполненный массив данных:");
+        System.out.println("\nЗадание 4.\nЗаполненный массив данных:");
         fillDiagArr(num4);
-        System.out.println();
 
         //задание 5
         //использую два метода для раздельного поиска мин и макс чисел в массиве
-        System.out.println("Задание 5.");
+        System.out.println("\nЗадание 5.\nИсходный массив данных:");
+        System.out.println(Arrays.toString(num5));
         minElementArr(num5);
         maxElementArr(num5);
-        System.out.println();
 
         //задание 6
-        System.out.println("Задание 6.");
-        System.out.print("в массиве есть место, в котором сумма левой и правой части равны - ");
-        System.out.println( checkBalance(num6) );
+        System.out.println("\nЗадание 6.\nПроверяемый массив данных:");
+        System.out.println(Arrays.toString(num6));
+        if( checkBalance(num6) ) {
+            System.out.println("в массиве ЕСТЬ место, в котором сумма левой и правой части равны (true)");
+        } else {
+            System.out.println("в массиве НЕТ места, в котором сумма левой и правой части равны (false)");
+        }
 
+        //задание 7
+        System.out.println("\nЗадание 7.\nИсходный массив данных:");
+        System.out.println(Arrays.toString(num7));
+        shiftArr(num7, -32);
+        System.out.println("Преобразованный массив данных:");
+        System.out.println(Arrays.toString(num7));
 
     }
 
@@ -67,7 +77,6 @@ public class Main {
                 arr[i] = 1;
             }
         }
-        System.out.println(Arrays.toString(arr));
     }
 
     //метод для задания 2
@@ -75,7 +84,6 @@ public class Main {
         for (int i = 1; i < arr.length; i++) {
             arr[i] = arr[i-1] + 3;    //заполняем массив арифметической прогрессией +3
         }
-        System.out.println(Arrays.toString(arr));
     }
 
     //метод для задания 3
@@ -85,7 +93,6 @@ public class Main {
                 arr[i] *= 2;
             }
         }
-        System.out.println(Arrays.toString(arr));
     }
 
     //метод для задания 4
@@ -136,5 +143,29 @@ public class Main {
         }
         return false;
     }
+
+    //метод для задания 7
+    public static void shiftArr(int[] arr, int n) {
+        int temp = 0;
+        n = n % arr.length;	//если число задали больше величины массива, то не будем сдвигать несколько раз по кругу
+        while(n != 0) {
+            if(n > 0) {
+                temp = arr[arr.length - 1];
+                for (int i = (arr.length - 1); i > 0; i--) {
+                    arr[i] = arr[i-1];
+                }
+                arr[0] = temp;
+                n--;
+            } else {
+                temp = arr[0];
+                for (int i = 0; i < (arr.length -1); i++) {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.length - 1] = temp;
+                n++;
+            }
+        }
+    }
+
 
 }
